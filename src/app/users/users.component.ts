412e2f2986c35fc009a752d82cbc227d06393747
ShymaaRagab <102189654+ShymaaRagab@users.noreply.github.com>
2024-10-30 23:55:03 +0300

    SH_R

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, OnChanges{
+  userData :any[]=[];
+
+  constructor(private userservice:UsersService){}
+
+  ngOnInit(): void {
+    this.userservice.getUsers().subscribe(
+      {
+        next:(res)=> this.userData = res.users
+      }
+    );
+  }
+
+  ngOnChanges(): void {
+    this.userData;
+  }
+  sortbyName(){
+    this.userData.sort((a, b) => {
+      if (a.firstname < b.firstname) return -1;
+      if (a.firstname > b.firstname) return 1;
+      return 0;
+    });
+    console.log(this.userData)
+    return this.userData;
+  }
 
 }
